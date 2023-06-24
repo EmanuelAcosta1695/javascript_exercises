6c7dc13bab4d12d800d37c9f06b6b5828c03a52f
Emanuel Acosta <e.acosta1695@gmail.com>
2023-06-24 11:54:33 -0300

    Primer commit de los ejercicios

---
+++ Readme.md
@@ -0,0 +1,3 @@
+Ejercicios en Javacript hechos para la materia "Desarrollo de Sistemas Web" (2023)  
+de la "Tecnicatura Superior en Desarrollo de Software", coorte: 2021.  
+I.S.F.D y T N°27
---
+++ tp_n2.js
@@ -0,0 +1,238 @@
+
+    /*
+    Ejercicio 1
+    Dado un array de números enteros, se deberá calcular el puntaje total en función de lo siguiente:
+    1. Suma 1 punto por cada número par del array.
+    2. Suma 3 puntos por cada número impar del array, excepto el valor 5.
+    3. Suma 5 puntos cada vez que aparezca el valor 5.
+    Por ejemplo:
+    Entrada: [1, 2, 3, 4, 5] 2+6+5
+    Resultado: 13
+    */
+
+
+    let suma = 0;
+    let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]; //7+5+21=33+1
+
+    nums.forEach(element => {
+    if (element%2 === 0){
+        console.log("PAR")
+        console.log("1")
+        suma += 1;
+    
+    } else if (element%2 !== 0) {
+        console.log("IMPAR")
+        
+        if (element === 5){
+            console.log("5")
+            suma += 5;
+        } else {
+            console.log("3")
+            suma += 3
+        }
+        
+    }
+    });
+
+    console.log(suma)
+
+
+    //   for (let i= 0; i<nums.length; i++) {
+    //     console.log("i=", i)
+    //     console.log("nums[i]=", nums[i])
+    //     console.log("   ")
+
+    //     if (nums[i]%2 === 0){
+    //         console.log("PAR")
+    //         suma += 1;
+
+    //     } else if (nums[i]%2 !== 0) {
+    //         console.log("IMPAR")
+    //       if (nums[i] === 5){
+    //         console.log("5")
+    //         suma += 5;
+    //       } else {
+    //         console.log("3")
+    //         suma += 3
+    //       }
+          
+    //     }
+
+    //     console.log("   ")
+    //   }
+
+    //   console.log(suma)
+
+
+
+    /* ---------------------------------------------------------------------------------- */
+
+
+
+    /*
+    Ejercicio 2
+    Completar las condiciones de los if del siguiente script para que los mensajes 
+    se muestren siempre de forma correcta:
+
+    let numero1 = 5;
+    let numero2 = 8;
+    if(...) {
+    console.log("numero1 no es mayor que numero2");
+    }
+    if(...) {
+    console.log("numero2 es positivo");
+    }
+    if(...) {
+    console.log("numero1 es negativo o distinto de cero");
+    }
+    if(...) {
+    console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+    }
+    */
+
+
+    let numero1 = 5;
+    let numero2 = 8;
+    if(numero1 < numero2) {
+        console.log("numero1 no es mayor que numero2");
+    }
+    if(numero2 > 0) {
+    console.log("numero2 es positivo");
+    }
+    if(numero1 != 0 || numero1 < 0) {
+    console.log("numero1 es negativo o distinto de cero");
+    }
+    if(numero1+1 < numero2) {
+    console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+    }
+
+
+
+    /* ---------------------------------------------------------------------------------- */
+
+
+
+    /*
+    Ejercicio 3
+    El cálculo de la letra del Documento Nacional de Identidad (DNI) es un proceso matemático sencillo 
+    que se basa en obtener el resto de la división entera entre el número de DNI y el número 23. 
+    A partir del resto de la división, se obtiene la letra seleccionándola dentro de un array de letras.
+    
+    El array de letras es:
+    let letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+    
+    Por tanto si el resto de la división es 0, la letra del DNI es la T y si el resto es 3 la letra es la A. 
+    
+    Con estos datos, elaborar un pequeño script que:
+        1. Almacene en una variable el número de DNI del usuario y en otra variable la letra del DNI.
+        2. En primer lugar, se debe comprobar si el número es menor que 0 o mayor que 99999999. Si ese es el caso, 
+           se muestra un mensaje al usuario indicando que el número proporcionado no es válido y el programa no muestra más mensajes.
+        3. Si el número es válido, se calcula la letra que le corresponde según el método explicado anteriormente.
+        4. Una vez calculada la letra, se debe comparar con la letra indicada por el usuario. Si no coinciden, 
+           se muestra un mensaje al usuario diciéndole que la letra que ha indicado no es correcta. En otro caso, 
+           se muestra un mensaje indicando que el número y la letra de DNI son correctos.
+    */
+
+
+    let letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+    //let numDni = prompt("Enter your DNI: ");
+    let numDni = 38672778;
+    let userLetter = "A"
+
+    if (numDni > 0 && numDni < 99999999) {
+        let restDivision = numDni%23;
+        let letterDni = letras[restDivision];
+
+        if (letterDni != userLetter) {
+            console.log("The letter that you enter is incorrect for you DNI number.")
+        } else {
+            console.log("The letter and DNI number that you enter is correct.")
+        }
+
+        // console.log(restDivision)
+        // console.log("Your letter for your DNI number is: ", letterDni)
+
+    } else {
+        console.log("DNI number invalid.") 
+    }
+       
+
+    /* ---------------------------------------------------------------------------------- */        
+
+
+    /*
+    Ejercicio 4
+    El factorial de un número entero n es una operación matemática que consiste en multiplicar 
+    todos los factores n x (n-1) x (n-2) …... Así, el factorial de 5 (escrito como 5!) es igual a: 5! = 5 x 4 x 3 x 2 x 1 = 120
+    Utilizando la estructura for, crear un script que calcule el factorial de un número entero.
+    */
+
+    let num = prompt("Enter a num: ")
+    let factorial = 1;
+
+    for (let i = 1; i<=num; i++) {
+        factorial *= i;
+    }
+
+    console.log(factorial)
+
+
+    /* ---------------------------------------------------------------------------------- */
+
+
+    /*
+    Ejercicio 5
+    Almacenar en una variable una cadena de texto e indicar si la misma es un palíndromo, es decir, 
+    si se lee de la misma forma desde la izquierda y desde la derecha. Ejemplo de palíndromo: "La ruta nos aporto otro paso natural".
+    */
+
+    let palindrome = "La ruta nos aporto otro paso natural";
+    let newPalindrome = palindrome.replace(/\s+/g, '').toLowerCase();
+    let reversePalindrome = palindrome.split("").reverse().join("").replace(/\s+/g, '').toLowerCase();
+
+    if (newPalindrome === reversePalindrome) {
+        console.log("El texto: '", palindrome, "' es palindromo.")
+    }
+
+    console.log(newPalindrome)
+    console.log(reversePalindrome)
+
+
+    /* ---------------------------------------------------------------------------------- */
+
+    
+    /*
+    Ejercicio 6
+    Escribir un script que simule el lanzamiento de dos dados. Hacer uso de la función Math.random para obtener números 
+    aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados y anotar en un array
+    el número de apariciones de dicha suma, repitiendo 36.000 veces esta operación. Mostrar un mensaje indicando cual es el valor 
+    que más veces se repitió.
+    */
+
+    let resultados = {};
+  
+    for (let i = 0; i < 36000; i++) {
+        let dice1 = Math.floor(Math.random() * 6) + 1;
+        let dice2 = Math.floor(Math.random() * 6) + 1;
+        let suma = dice1 + dice2;
+        
+        if (resultados.hasOwnProperty(suma)) {
+            resultados[suma]++;
+        } else {
+            resultados[suma] = 1;
+        }
+    }
+
+    console.log(resultados)
+    
+    let numMaxSuma = 0;
+    let maxApariciones = 0;
+    
+    for (let suma in resultados) {
+        if (resultados[suma] > maxApariciones) {
+            maxApariciones = resultados[suma];
+            numMaxSuma = suma;
+        }
+    }
+    
+    console.log(`El valor que más veces se repitió es ${numMaxSuma} con ${maxApariciones} apariciones.`);
